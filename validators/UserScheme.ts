f42933848286fd770a22a47ca6f08b1d5b65a3cf
Piotr Jarolewski <piotr.jarolewski@tsh.io>
2020-06-16 12:50:24 +0200

    Delete old code

--- validators/UserScheme.ts
+++ validators/UserScheme.ts
@@ -12,13 +12,7 @@ export const emailExists = (): Validator => {
     type: "emailExists",
     check: async (value: any) => {
       const user = await userRepository.findUserByEmail(value);
-      // const result = await pgClient.query(
-      //   "SELECT COUNT(*) FROM users WHERE email = lower($1)",
-      //   value,
-      // );
-      // const rows = result.rowsOfObjects();
       return (user === null) ? undefined : {};
-      // return (rows[0]["count"] != 0) ? {} : undefined;
     },
     message: (value: any, args?: Args) => {
       return `User with email [${value}] already exists.`;
